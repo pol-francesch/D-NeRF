4c379889d67c681be39c9691a562a400f09cd7bf
Pol Francesch <pfrances@purdue.edu>
2023-05-10 15:42:36 -0700

    modified the sherlock bash script

--- sherlock/train_shirt.sh
+++ sherlock/train_shirt.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=train_shirt_roe2
 #SBATCH --output=train_shirt_roe2.%j.out
 #SBATCH --error=train_shirt_roe2.%j.err
-#SBATCH --time=24:00:00
+#SBATCH --time=28:00:00
 #SBATCH --ntasks=1
 #SBATCH -p gpu
 #SBATCH -G 1
@@ -10,9 +10,10 @@
 #SBATCH --mem=16GB
 #SBATCH -C GPU_MEM:16GB
 
-module load gcc/9.1.0
-module load python/3.9.0
+module reset
+module load py-pytorch/2.0 py-torchvision/0.15.1_py39
+module load py-matplotlib/3.7.1_py39
 cd $HOME
-source python_envs/dnerf/bin/activate
+source $HOME/python_envs/dnerf/bin/activate
 
 python $HOME/D-NeRF/run_dnerf.py --config $HOME/D-NeRF/configs/sherlock/shirt_roe2.txt
